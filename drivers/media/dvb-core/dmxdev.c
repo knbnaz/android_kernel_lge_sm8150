36827364d821de94ebc63b4e4d409a06a27390a6
Al Viro <viro@zeniv.linux.org.uk>
2017-07-03 03:02:56 -0400

    media: annotate ->poll() instances
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/media/dvb-core/dmxdev.c
+++ drivers/media/dvb-core/dmxdev.c
@@ -4509,10 +4509,10 @@ static long dvb_demux_compat_ioctl(struct file *file, unsigned int cmd,
 }
 #endif
 
-static unsigned int dvb_demux_poll(struct file *file, poll_table *wait)
+static __poll_t dvb_demux_poll(struct file *file, poll_table *wait)
 {
 	struct dmxdev_filter *dmxdevfilter = file->private_data;
-	unsigned int mask = 0;
+	__poll_t mask = 0;
 
 	if (!dmxdevfilter)
 		return -EINVAL;
@@ -4710,11 +4710,11 @@ static long dvb_dvr_compat_ioctl(struct file *file, unsigned int cmd,
 }
 #endif
 
-static unsigned int dvb_dvr_poll(struct file *file, poll_table *wait)
+static __poll_t dvb_dvr_poll(struct file *file, poll_table *wait)
 {
 	struct dvb_device *dvbdev = file->private_data;
 	struct dmxdev *dmxdev = dvbdev->priv;
-	unsigned int mask = 0;
+	__poll_t mask = 0;
 
 	if ((file->f_flags & O_ACCMODE) == O_RDONLY) {
 		poll_wait(file, &dmxdev->dvr_buffer.queue, wait);
