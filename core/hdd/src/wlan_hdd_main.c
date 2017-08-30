OnePlusOSS/android_kernel_oneplus_msm8998

qcacld-3.0: do SAP internal restart based on INI
On MDM platform, scm or wlan_services will take care
the Hostapd restart when get LTE coex event.
To make it configurable to avoid duplication of restart
SAP.

Change-Id: Idfb0b6967643d85315df23264a38fc68863a9fea
CRs-Fixed: 2073634

core/hdd/src/wlan_hdd_main.c
@@ -6891,7 +6891,12 @@ void hdd_unsafe_channel_restart_sap(hdd_context_t *hdd_ctxt)
 			hdd_debug("sending coex indication");
 			wlan_hdd_send_svc_nlink_msg(hdd_ctxt->radio_index,
 					WLAN_SVC_LTE_COEX_IND, NULL, 0);
-			hdd_restart_sap(adapter_temp, restart_chan);
+			hdd_debug("driver to start sap: %d",
+				hdd_ctxt->config->sap_internal_restart);
+			if (hdd_ctxt->config->sap_internal_restart)
+				hdd_restart_sap(adapter_temp, restart_chan);
+			else
+				return;
 		}
 
 next_adapater:

knbnaz/android_kernel_lge_sm8150

core/hdd/src/wlan_hdd_main.c
@@ -7156,7 +7156,12 @@ void hdd_unsafe_channel_restart_sap(hdd_context_t *hdd_ctxt)
 			hdd_debug("sending coex indication");
 			wlan_hdd_send_svc_nlink_msg(hdd_ctxt->radio_index,
 					WLAN_SVC_LTE_COEX_IND, NULL, 0);
-			hdd_switch_sap_channel(adapter_temp, restart_chan);
+			hdd_debug("driver to start sap: %d",
+				hdd_ctxt->config->sap_internal_restart);
+			if (hdd_ctxt->config->sap_internal_restart)
+				hdd_switch_sap_channel(adapter_temp, restart_chan);
+			else
+				return;
 		}
 
 next_adapater: