knbnaz/android_kernel_lge_sm8150

commit 50085d66865276f95b5ec7fc2db86aa81ac06251

mptcp: Reset sk_prot_creator to the correct protocol
sk->sk_prot and sk->sk_prot_creator can differ when the app uses
IPV6_ADDRFORM (transforming an IPv6-socket to an IPv4-one). When the app
does this on a meta-socket, subsequent subflows will be created as pure
IPv4 sockets, but sk_prot_creator will still point to tcpv6_prot.

When freeing, we will thus put this tcp_sock back in to the slab of
tcpv6_prot. This will lead to memory-corruption as the slab
allocator might write at the end of the allocated region.

It's easy to repro with packetdrill on a build that has MEMCG_KMEM and
slub-debugging enabled:

--ip_version="ipv4-mapped-ipv6"
--mtu=1520

+0 socket(..., SOCK_STREAM, IPPROTO_TCP) = 3
+0  setsockopt(3, SOL_SOCKET, SO_REUSEADDR, [1], 4) = 0
+0  bind(3, {sa_family = AF_INET6, sin_port = htons(13000), sin_addr = inet_addr("::")}, ...) = 0
+0  listen(3, 1) = 0

+0  socket(..., SOCK_STREAM, IPPROTO_TCP) = 5
+0  setsockopt(5, SOL_SOCKET, SO_REUSEADDR, [1], 4) = 0
+0  bind(5, {sa_family = AF_INET6, sin_port = htons(13001), sin_addr = inet_addr("::")}, ...) = 0
+0  listen(5,1) = 0

+0  < S 0:0(0) win 32792 <mss 1460,sackOK,nop,nop,nop,wscale 7,mp_capable key_a> sock(3)
+0  > S. 0:0(0) ack 1 win 28800 <mss 1460,nop,nop,sackOK,nop,wscale 7,mp_capable key_b> sock(3)
+0  < . 1:1(0) ack 1 win 257 <mp_capable key_a key_b> sock(3)
+0  accept(3, ..., ...) = 4

+0.1    setsockopt(4, SOL_IPV6, 1, [AF_INET], 4) = 0

//First subflow
+0.1  < S 0:0(0) win 32792 <mss 1460,sackOK,nop,nop,nop,wscale 7,mp_join_syn backup=0 address_id=0 token=sha1_32(key_b)> sock(5)
+0  > S. 0:0(0) ack 1 win 28800 <mss 1460,nop,nop,sackOK,nop,wscale 7, mp_join_syn_ack backup=0 address_id=0 sender_hmac=trunc_l64_hmac(key_b key_a) > sock(5)

+0  < . 1:1(0) ack 1 win 32792 <mp_join_ack sender_hmac=full_160_hmac(key_a key_b)> sock(5)
+0 mp_join_accept(5) = 6

+0 > . 1:1(0) ack 1 <...> sock(6) // reliably mp_join_ack
+0  < . 1:1(0) ack 1 win 32792 sock(6)

net/core/sock.c
@@ -1356,6 +1356,7 @@ struct sock *sk_prot_alloc(struct proto *prot, gfp_t priority,
 
 	if (sk != NULL) {
 		kmemcheck_annotate_bitfield(sk, flags);
+		sk->sk_prot_creator = prot;
 
 		if (security_sk_alloc(sk, family, priority))
 			goto out_free;
@@ -1512,6 +1513,8 @@ struct sock *sk_clone_lock(const struct sock *sk, const gfp_t priority)
 		struct sk_filter *filter;
 
 		sock_copy(newsk, sk);
+
+		newsk->sk_prot_creator = sk->sk_prot;
 
 		/* SANITY */
 		if (likely(newsk->sk_net_refcnt))

commit 17d99a6f13913ff8a00a567fe7c991765de41293

mptcp: Remove unnecessary sk_prot_creator assignment
While submitting to upstream, I realize that this assignment is not
necessary, as the sock has not yet been initialized (all fields are
anyways bogus at this point here).

So, no need to set sk_prot_creator inside sk_prot_alloc()

Fixes: 50085d668652 (mptcp: Reset sk_prot_creator to the correct protocol)
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/core/sock.c
@@ -1356,7 +1356,6 @@ struct sock *sk_prot_alloc(struct proto *prot, gfp_t priority,
 
 	if (sk != NULL) {
 		kmemcheck_annotate_bitfield(sk, flags);
-		sk->sk_prot_creator = prot;
 
 		if (security_sk_alloc(sk, family, priority))
 			goto out_free;