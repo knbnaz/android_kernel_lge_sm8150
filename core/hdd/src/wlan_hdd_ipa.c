MotorolaMobilityLLC/vendor-qcom-opensource-wlan-qcacld-3.0

qcacld-3.0: Add sanity check on adapter before clean-up
Add sanity check on the adapter structure, using the magic field
before accessing any fields on it.

Change-Id: If6965415bc0e6400ffb5924461715dff4c9efa36
CRs-Fixed: 2054084

core/hdd/src/wlan_hdd_ipa.c
@@ -5052,6 +5052,13 @@ static void hdd_ipa_cleanup_iface(struct hdd_ipa_iface_context *iface_context)
 {
 	if (iface_context == NULL)
 		return;
+	if (iface_context->adapter->magic != WLAN_HDD_ADAPTER_MAGIC) {
+		HDD_IPA_LOG(QDF_TRACE_LEVEL_DEBUG,
+			    "%s: bad adapter(%p).magic(%d)!",
+			    __func__, iface_context->adapter,
+			    iface_context->adapter->magic);
+		return;
+	}
 
 	hdd_ipa_clean_hdr(iface_context->adapter);
 

knbnaz/android_kernel_lge_sm8150

core/hdd/src/wlan_hdd_ipa.c
@@ -4728,6 +4728,13 @@ static void hdd_ipa_cleanup_iface(struct hdd_ipa_iface_context *iface_context)
 
 	if (iface_context == NULL)
 		return;
+	if (iface_context->adapter->magic != WLAN_HDD_ADAPTER_MAGIC) {
+		HDD_IPA_LOG(QDF_TRACE_LEVEL_DEBUG,
+			    "%s: bad adapter(%pK).magic(%d)!",
+			    __func__, iface_context->adapter,
+			    iface_context->adapter->magic);
+		return;
+	}
 
 	cdp_ipa_cleanup_iface(cds_get_context(QDF_MODULE_ID_SOC),
 		      iface_context->adapter->dev->name,