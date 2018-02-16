bf9f05e55d33841d8eeb7686ed611c26215d6247
Shunyong Yang <shunyong.yang@hxt-semitech.com>
2018-02-16 23:07:09 +0200

    lib/vsprintf: Replace space with '_' before crng is ready
    Before crng is ready, output of "%p" composes of "(ptrval)" and
    left padding spaces for alignment as no random address can be
    generated. This seems a little strange when default string width
    is larger than strlen("(ptrval)").
    
    For example, when irq domain names are built with "%p", the nodes
    under /sys/kernel/debug/irq/domains like this on AArch64 system,
    
    [root@y irq]# ls domains/
    default                   irqchip@        (ptrval)-2
    irqchip@        (ptrval)-4  \_SB_.TCS0.QIC1  \_SB_.TCS0.QIC3
    irqchip@        (ptrval)  irqchip@        (ptrval)-3
    \_SB_.TCS0.QIC0             \_SB_.TCS0.QIC2
    
    The name "irqchip@        (ptrval)-2" is not so readable in console
    output.
    
    This patch replaces space with readable "_" when output needs padding.
    Following is the output after applying the patch,
    
    [root@y domains]# ls
    default                   irqchip@(____ptrval____)-2
    irqchip@(____ptrval____)-4  \_SB_.TCS0.QIC1  \_SB_.TCS0.QIC3
    irqchip@(____ptrval____)  irqchip@(____ptrval____)-3  \_SB_.TCS0.QIC0
    \_SB_.TCS0.QIC2
    
    There is same problem in some subsystem's dmesg output. Moreover,
    someone may call "%p" in a similar case. In addition, the timing of
    crng initialization done may vary on different system. So, the change
    is made in vsprintf.c.
    
    Suggested-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Link: http://lkml.kernel.org/r/20180216210711.79901-7-andriy.shevchenko@linux.intel.com
    To: "Tobin C . Harding" <me@tobin.cc>
    To: linux@rasmusvillemoes.dk
    To: Joe Perches <joe@perches.com>
    To: linux-kernel@vger.kernel.org
    To: Andrew Morton <akpm@linux-foundation.org>
    Cc: Joey Zheng <yu.zheng@hxt-semitech.com>
    Signed-off-by: Shunyong Yang <shunyong.yang@hxt-semitech.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Petr Mladek <pmladek@suse.com>

--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -686,12 +686,13 @@ early_initcall(initialize_ptr_random);
 static char *ptr_to_id(char *buf, char *end, const void *ptr,
 		       struct printf_spec spec)
 {
+	const char *str = sizeof(ptr) == 8 ? "(____ptrval____)" : "(ptrval)";
 	unsigned long hashval;
 
 	if (static_branch_unlikely(&not_filled_random_ptr_key)) {
 		spec.field_width = 2 * sizeof(ptr);
 		/* string length must be less than default_width */
-		return string(buf, end, "(ptrval)", spec);
+		return string(buf, end, str, spec);
 	}
 
 #ifdef CONFIG_64BIT
