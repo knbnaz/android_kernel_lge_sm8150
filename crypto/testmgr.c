6a151ab9e418b7d5cf53d6108d32a46eb99a2aab
Gilad Ben-Yossef <gilad@benyossef.com>
2018-03-06 09:44:43 +0000

    crypto: testmgr - introduce SM4 tests
    Add testmgr tests for the newly introduced SM4 ECB symmetric cipher.
    
    Signed-off-by: Gilad Ben-Yossef <gilad@benyossef.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -3089,6 +3089,15 @@ static const struct alg_test_desc alg_test_descs[] = {
 			}
 		}
 	}, {
+		.alg = "ecb(sm4)",
+		.test = alg_test_skcipher,
+		.suite = {
+			.cipher = {
+				.enc = __VECS(sm4_enc_tv_template),
+				.dec = __VECS(sm4_dec_tv_template)
+			}
+		}
+	}, {
 		.alg = "ecb(tea)",
 		.test = alg_test_skcipher,
 		.suite = {
