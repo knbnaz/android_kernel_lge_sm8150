0e3111716a18d8b2002e05606368b9a5b9188ffc
Tobin C. Harding <me@tobin.cc>
2018-06-22 09:15:33 +1000

    vsprintf: Use hw RNG for ptr_key
    Currently we must wait for enough entropy to become available before
    hashed pointers can be printed.  We can remove this wait by using the
    hw RNG if available.
    
    Use hw RNG to get keying material.
    
    Reviewed-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Suggested-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Tobin C. Harding <me@tobin.cc>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -678,8 +678,16 @@ static struct notifier_block random_ready = {
 
 static int __init initialize_ptr_random(void)
 {
-	int ret = register_random_ready_notifier(&random_ready);
+	int key_size = sizeof(ptr_key);
+	int ret;
 
+	/* Use hw RNG if available. */
+	if (get_random_bytes_arch(&ptr_key, key_size) == key_size) {
+		static_branch_disable(&not_filled_random_ptr_key);
+		return 0;
+	}
+
+	ret = register_random_ready_notifier(&random_ready);
 	if (!ret) {
 		return 0;
 	} else if (ret == -EALREADY) {
