e7e179e559417d1960e7ef5584ffad8a3a8a3e24
Al Viro <viro@zeniv.linux.org.uk>
2020-01-19 11:44:51 -0500

    sanitize handling of nd->last_type, kill LAST_BIND
    ->last_type values are set in 3 places: path_init() (sets to LAST_ROOT),
    link_path_walk (LAST_NORM/DOT/DOTDOT) and pick_link (LAST_BIND).
    
    The are checked in walk_component(), lookup_last() and do_last().
    They also get copied to the caller by filename_parentat().  In the last
    3 cases the value is what we had at the return from link_path_walk().
    In case of walk_component() it's either directly downstream from
    assignment in link_path_walk() or, when called by lookup_last(), the
    value we have at the return from link_path_walk().
    
    The value at the entry into link_path_walk() can survive to return only
    if the pathname contains nothing but slashes.  Note that pick_link()
    never returns such - pure jumps are handled directly.  So for the calls
    of link_path_walk() for trailing symlinks it does not matter what value
    had been there at the entry; the value at the return won't depend upon it.
    
    There are 3 call chains that might have pick_link() storing LAST_BIND:
    
    1) pick_link() from step_into() from walk_component() from
    link_path_walk().  In that case we will either be parsing the next
    component immediately after return into link_path_walk(), which will
    overwrite the ->last_type before anyone has a chance to look at it,
    or we'll fail, in which case nobody will be looking at ->last_type at all.
    
    2) pick_link() from step_into() from walk_component() from lookup_last().
    The value is never looked at due to the above; it won't affect the value
    seen at return from any link_path_walk().
    
    3) pick_link() from step_into() from do_last().  Ditto.
    
    In other words, assignemnt in pick_link() is pointless, and so is
    LAST_BIND itself; nothing ever looks at that value.  Kill it off.
    And make link_path_walk() _always_ assign ->last_type - in the only
    case when the value at the entry might survive to the return that value
    is always LAST_ROOT, inherited from path_init().  Move that assignment
    from path_init() into the beginning of link_path_walk(), to consolidate
    the things.
    
    Historical note: LAST_BIND used to be used for the kludge with trailing
    pure jump symlinks (extra iteration through the top-level loop).
    No point keeping it anymore...
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/namei.c
+++ fs/namei.c
@@ -1869,7 +1869,6 @@ static const char *pick_link(struct nameidata *nd, struct path *link,
 	if (unlikely(error))
 		return ERR_PTR(error);
 
-	nd->last_type = LAST_BIND;
 	res = READ_ONCE(inode->i_link);
 	if (!res) {
 		const char * (*get)(struct dentry *, struct inode *,
@@ -2207,6 +2206,7 @@ static int link_path_walk(const char *name, struct nameidata *nd)
 {
 	int err;
 
+	nd->last_type = LAST_ROOT;
 	if (IS_ERR(name))
 		return PTR_ERR(name);
 	while (*name=='/')
@@ -2308,7 +2308,6 @@ static const char *path_init(struct nameidata *nd, unsigned flags)
 	if (flags & LOOKUP_RCU)
 		rcu_read_lock();
 
-	nd->last_type = LAST_ROOT; /* if there are only slashes... */
 	nd->flags = flags | LOOKUP_JUMPED | LOOKUP_PARENT;
 	nd->depth = 0;
 
