maltesermailo/android_kernel_pantah

ANDROID: rwsem: Add vendor hook to the rw-semaphore
  - Add the hook to apply vendor's performance tune for owner
    of rwsem.

  - Add the hook for the waiter list of rwsem to allow
    vendor perform waiting queue enhancement

  - ANDROID_VENDOR_DATA added to rw_semaphore

Bug: 161400830

Signed-off-by: Jian-Min Liu <jian-min.liu@mediatek.com>
Change-Id: I007a5e26f3db2adaeaf4e5ccea414ce7abfa83b8

drivers/android/vendor_hooks.c
@@ -11,6 +11,7 @@
 #include <trace/hooks/sched.h>
 #include <trace/hooks/fpsimd.h>
 #include <trace/hooks/binder.h>
+#include <trace/hooks/rwsem.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -27,3 +28,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_find_lowest_rq);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_is_fpsimd_save);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_set_tune);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_restore_tune);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_set_tune);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_restore_tune);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alter_rwsem_list_add);

knbnaz/android_kernel_lge_sm8150

Signed-off-by: JianMin Liu <jian-min.liu@mediatek.com>

drivers/android/vendor_hooks.c
@@ -12,6 +12,7 @@
 #include <trace/hooks/dtask.h>
 #include <trace/hooks/net.h>
 #include <trace/hooks/binder.h>
+#include <trace/hooks/rwsem.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -30,3 +31,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_kfree_skb);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_transaction_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_set_priority);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_restore_priority);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_init);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_wake);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_write_finished);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alter_rwsem_list_add);