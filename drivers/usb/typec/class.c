MotorolaMobilityLLC/kernel-msm

usb: typec: Fix setting of invalid value of opmode
Setting opmode to invalid values would lead to a
paging fault failure when there is an access to the
power_operation_mode.

Prevent this by checking the validity of the value
that the opmode is being set.

Change-Id: I9ada1ad4aeeca08d3072a5738f5fa48537a2cecf
Signed-off-by: Sriharsha Allenki <sallenki@codeaurora.org>

drivers/usb/typec/class.c
@@ -1378,7 +1378,8 @@ void typec_set_pwr_opmode(struct typec_port *port,
 {
 	struct device *partner_dev;
 
-	if (port->pwr_opmode == opmode)
+	if ((port->pwr_opmode == opmode) || (opmode < TYPEC_PWR_MODE_USB) ||
+						(opmode > TYPEC_PWR_MODE_MAX))
 		return;
 
 	port->pwr_opmode = opmode;

knbnaz/android_kernel_lge_sm8150

Change-Id: I9863df1e84a947fb04e29d214c1dec61ccc5ee72

drivers/usb/typec/class.c
@@ -1369,7 +1369,8 @@ void typec_set_pwr_opmode(struct typec_port *port,
 {
 	struct device *partner_dev;
 
-	if (port->pwr_opmode == opmode)
+	if ((port->pwr_opmode == opmode) || (opmode < TYPEC_PWR_MODE_USB) ||
+						(opmode > TYPEC_PWR_MODE_PD))
 		return;
 
 	port->pwr_opmode = opmode;