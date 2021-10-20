knbnaz/android_kernel_lge_sm8150

commit 527a937f5e94beed408ba38f126d9b5282949ab0

mptcp: Allow client-side-only and server-side-only sysctl configuration
Make mptcp_enabled rather a bitfield with the following semantics:

0x01: Enable MPTCP (globally)
0x04: Disable MPTCP on Clients
0x08: Disable MPTCP on Servers
0x02: Disabled, and only can be enabled through the socket-option

This allows to deploy MPTCP such that only incoming connections get
MPTCP by setting the sysctl to 0x05. If you want to only allow outgoing
ones to use MPTCP, set it to 0x09.

This is very useful if the binary can't be changed (e.g., make it use
the setsockopt).

Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

include/net/mptcp.h
@@ -689,7 +689,10 @@ static inline int mptcp_sub_len_dss(const struct mp_dss *m, const int csum)
 	return 4 + m->A * (4 + m->a * 4) + m->M * (10 + m->m * 4 + csum * 2);
 }
 
-#define MPTCP_SYSCTL	1
+#define MPTCP_ENABLE		0x01
+#define MPTCP_SOCKOPT		0x02
+#define MPTCP_CLIENT_DISABLE	0x04
+#define MPTCP_SERVER_DISABLE	0x08
 
 extern int sysctl_mptcp_enabled;
 extern int sysctl_mptcp_version;
@@ -949,7 +952,25 @@ extern struct mptcp_sched_ops mptcp_sched_default;
 /* Initializes function-pointers and MPTCP-flags */
 static inline void mptcp_init_tcp_sock(struct sock *sk)
 {
-	if (!mptcp_init_failed && sysctl_mptcp_enabled == MPTCP_SYSCTL)
+	if (!mptcp_init_failed && sysctl_mptcp_enabled == MPTCP_ENABLE)
+		mptcp_enable_sock(sk);
+}
+
+static inline void mptcp_init_listen(struct sock *sk)
+{
+	if (!mptcp_init_failed &&
+	    sk->sk_type == SOCK_STREAM && sk->sk_protocol == IPPROTO_TCP &&
+	    sysctl_mptcp_enabled & MPTCP_ENABLE &&
+	    !(sysctl_mptcp_enabled & MPTCP_SERVER_DISABLE))
+		mptcp_enable_sock(sk);
+}
+
+static inline void mptcp_init_connect(struct sock *sk)
+{
+	if (!mptcp_init_failed &&
+	    sk->sk_type == SOCK_STREAM && sk->sk_protocol == IPPROTO_TCP &&
+	    sysctl_mptcp_enabled & MPTCP_ENABLE &&
+	    !(sysctl_mptcp_enabled & MPTCP_CLIENT_DISABLE))
 		mptcp_enable_sock(sk);
 }
 
@@ -1525,6 +1546,8 @@ static inline void mptcp_hash_remove_bh(struct tcp_sock *meta_tp) {}
 static inline void mptcp_remove_shortcuts(const struct mptcp_cb *mpcb,
 					  const struct sk_buff *skb) {}
 static inline void mptcp_init_tcp_sock(struct sock *sk) {}
+static inline void mptcp_init_listen(struct sock *sk) {}
+static inline void mptcp_init_connect(struct sock *sk) {}
 static inline void mptcp_disable_static_key(void) {}
 static inline void mptcp_cookies_reqsk_init(struct request_sock *req,
 					    struct mptcp_options_received *mopt,

net/ipv4/af_inet.c
@@ -231,6 +231,8 @@ int inet_listen(struct socket *sock, int backlog)
 			tcp_fastopen_init_key_once(sock_net(sk));
 		}
 
+		mptcp_init_listen(sk);
+
 		err = inet_csk_listen_start(sk, backlog);
 		if (err)
 			goto out;

net/ipv4/tcp_ipv4.c
@@ -211,6 +211,8 @@ int tcp_v4_connect(struct sock *sk, struct sockaddr *uaddr, int addr_len)
 	struct ip_options_rcu *inet_opt;
 	struct inet_timewait_death_row *tcp_death_row = &sock_net(sk)->ipv4.tcp_death_row;
 
+	mptcp_init_connect(sk);
+
 	if (addr_len < sizeof(struct sockaddr_in))
 		return -EINVAL;
 

net/ipv6/tcp_ipv6.c
@@ -150,6 +150,8 @@ int tcp_v6_connect(struct sock *sk, struct sockaddr *uaddr,
 	int err;
 	struct inet_timewait_death_row *tcp_death_row = &sock_net(sk)->ipv4.tcp_death_row;
 
+	mptcp_init_connect(sk);
+
 	if (addr_len < SIN6_LEN_RFC2133)
 		return -EINVAL;
 

commit 8cc08c42f162dbb4008f36f5f11be72f9936bf8a

mptcp: Fix TCP_MD5 and MPTCP coexistence
Coexistence of TCP_MD5 and MPTCP broke again. This is because now there
is yet another path through which MPTCP can be enabled, since commit
527a937f5e94 ("mptcp: Allow client-side-only and server-side-only sysctl configuration").

So, let's make sure we properly test for TCP_MD5 in all the possible
code-paths that end up calling mptcp_enable_sock.

Fixes: 527a937f5e94 ("mptcp: Allow client-side-only and server-side-only sysctl configuration")
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

include/net/mptcp.h
@@ -962,6 +962,9 @@ static inline void mptcp_init_listen(struct sock *sk)
 {
 	if (!mptcp_init_failed &&
 	    sk->sk_type == SOCK_STREAM && sk->sk_protocol == IPPROTO_TCP &&
+#ifdef CONFIG_TCP_MD5SIG
+	    !rcu_access_pointer(tcp_sk(sk)->md5sig_info) &&
+#endif
 	    sysctl_mptcp_enabled & MPTCP_ENABLE &&
 	    !(sysctl_mptcp_enabled & MPTCP_SERVER_DISABLE))
 		mptcp_enable_sock(sk);
@@ -971,6 +974,9 @@ static inline void mptcp_init_connect(struct sock *sk)
 {
 	if (!mptcp_init_failed &&
 	    sk->sk_type == SOCK_STREAM && sk->sk_protocol == IPPROTO_TCP &&
+#ifdef CONFIG_TCP_MD5SIG
+	    !rcu_access_pointer(tcp_sk(sk)->md5sig_info) &&
+#endif
 	    sysctl_mptcp_enabled & MPTCP_ENABLE &&
 	    !(sysctl_mptcp_enabled & MPTCP_CLIENT_DISABLE))
 		mptcp_enable_sock(sk);

net/ipv4/tcp.c
@@ -3305,7 +3305,7 @@ static int do_tcp_setsockopt(struct sock *sk, int level,
 		if (mptcp_init_failed || !sysctl_mptcp_enabled ||
 		    sk->sk_state != TCP_CLOSE
 #ifdef CONFIG_TCP_MD5SIG
-		    || tp->md5sig_info
+		    || rcu_access_pointer(tp->md5sig_info)
 #endif
 								) {
 			err = -EPERM;