e99291d5d592969c5e29784cfc8b74f7c8479fd7
Kendrenogen <senabuduh4ckerz@gmail.com>
2024-01-11 16:16:23 -0700

    fs: Change path_mounted Definition Back to the Old One
    path_mounted is not defined until commit 78aa08a8cab6731ab8f8241a9eb0b5021a648dd6
    (fs: add path_mounted()), so let's revert back to the old definition.
    
    Signed-off-by: Kendrenogen <senabuduh4ckerz@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1745,7 +1745,7 @@ static int can_umount(const struct path *path, int flags)
 
 	if (!may_mount())
 		return -EPERM;
-	if (!path_mounted(path))
+	if (path->dentry != path->mnt->mnt_root)
 		return -EINVAL;
 	if (!check_mnt(mnt))
 		return -EINVAL;
