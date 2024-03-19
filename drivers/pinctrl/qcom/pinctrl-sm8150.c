e61c1fa89a2e62f42da1886a4a4fb75a8009404d
Kendrenogen <senabuduh4ckerz@gmail.com>
2024-03-18 19:55:34 -0600

    qcom: pinctrl: Add LGE Changes to pinctrl

--- drivers/pinctrl/qcom/pinctrl-sm8150.c
+++ drivers/pinctrl/qcom/pinctrl-sm8150.c
@@ -5,6 +5,7 @@
 #include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/pinctrl/pinctrl.h>
+#include <linux/slab.h>
 
 #include "pinctrl-msm.h"
 
@@ -1599,6 +1600,22 @@ static int sm8150_pinctrl_gpio_irq_map_probe(struct platform_device *pdev)
 	return 0;
 }
 
+#ifdef CONFIG_LGE_PM
+static int *access_denied_list;
+static int access_denied_cnt;
+
+static bool msm_gpio_check_access(int gpio)
+{
+	int i = 0;
+
+	for (i = 0; i < access_denied_cnt; i++) {
+		if (access_denied_list[i] == gpio)
+			return false;
+	}
+	return true;
+}
+#endif
+
 static int sm8150_pinctrl_dirconn_list_probe(struct platform_device *pdev)
 {
 	int ret, n, dirconn_list_count, m;
@@ -1632,6 +1649,68 @@ static int sm8150_pinctrl_dirconn_list_probe(struct platform_device *pdev)
 
 static int sm8150_pinctrl_probe(struct platform_device *pdev)
 {
+#ifdef CONFIG_LGE_PM
+	int len = 0;
+	int rc = 0;
+	int i = 0;
+	int ret = 0;
+	struct property *gpios;
+
+	if (!pdev->dev.of_node) {
+		goto exit;
+	}
+
+	if (of_find_property(pdev->dev.of_node, "dirconn-list", &len)) {
+		ret = sm8150_pinctrl_dir_conn_probe(pdev);
+		if (ret) {
+			dev_err(&pdev->dev,
+				"Unable to parse TLMM direct connects\n");
+			goto exit;
+		}
+	}
+
+	gpios = of_find_property(pdev->dev.of_node, "lge,access-denied-gpios", &len);
+	if (!gpios) {
+		printk(KERN_ERR "cannot get access-denied-gpios property\n");
+		goto exit;
+	}
+
+	len /= sizeof(u32);
+	if (len < 1) {
+		printk(KERN_ERR "access-denied-gpios length is abnormal, %d\n", len);
+		goto exit;
+	}
+
+	printk(KERN_ERR "lge access-denied-gpios, %d\n", len);
+
+	access_denied_list = devm_kmalloc_array(&pdev->dev, len, sizeof(int), GFP_KERNEL);
+	if (!access_denied_list) {
+		printk(KERN_ERR "access_denied_list array malloc failed\n");
+		goto exit_malloc;
+	}
+
+	rc = of_property_read_u32_array(pdev->dev.of_node,
+			"lge,access-denied-gpios", access_denied_list, len);
+	if (rc) {
+		printk(KERN_ERR "property array read fail, %d\n", rc);
+		goto exit_malloc;
+	}
+
+	for (i = 0; i < len; i++)
+		printk(KERN_ERR "access_denied_list[%d] = %d\n", i, access_denied_list[i]);
+
+	access_denied_cnt = len;
+	return msm_pinctrl_probe(pdev, &sm8150_pinctrl);
+
+exit_malloc:
+	kfree(access_denied_list);
+exit:
+	access_denied_list = NULL;
+	access_denied_cnt = 0;
+
+
+	return msm_pinctrl_probe(pdev, &sm8150_pinctrl);
+#else
 	int len, ret;
 
 	if (of_find_property(pdev->dev.of_node, "qcom,gpio-irq-map", &len)) {
@@ -1653,6 +1732,7 @@ static int sm8150_pinctrl_probe(struct platform_device *pdev)
 	}
 
 	return msm_pinctrl_probe(pdev, &sm8150_pinctrl);
+#endif
 }
 
 static const struct of_device_id sm8150_pinctrl_of_match[] = {
