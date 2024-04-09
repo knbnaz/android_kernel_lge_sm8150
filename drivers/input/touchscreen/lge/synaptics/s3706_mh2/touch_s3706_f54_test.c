5d7dea5e099e8e1ea4980d0822227d9715da22fb
Kendrenogen <senabuduh4ckerz@gmail.com>
2024-04-09 17:56:37 -0600

    touchscreen: lge: Remove do_gettimeofday Usage

--- drivers/input/touchscreen/lge/synaptics/s3706_mh2/touch_s3706_f54_test.c
+++ drivers/input/touchscreen/lge/synaptics/s3706_mh2/touch_s3706_f54_test.c
@@ -369,7 +369,7 @@ static uint8_t RxPhysical[TRX_MAPPING_MAX];
 static uint8_t ExtendRT26_pin[4] = {0, 1, 32, 33};
 static int16_t TransRxShortData[TRX_MAX];
 
-struct timeval t_interval[TIME_PROFILE_MAX];
+struct timespec64 t_interval[TIME_PROFILE_MAX];
 static int f54len;
 static char f54buf[BUF_SIZE] = {0};
 
@@ -1379,7 +1379,7 @@ static int ReadReport(struct device *dev, u16 input, int mode, char *buf)
 		goto error;
 	}
 
-	do_gettimeofday(&t_interval[ENDTIME]);
+	ktime_get_real_ts64(&t_interval[ENDTIME]);
 
 	TOUCH_I("Takes %lu ticks\n",
 			get_time_interval(t_interval[ENDTIME].tv_sec,
