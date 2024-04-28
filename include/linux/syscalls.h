b250e8d7a38ccc4e717423bbb562a03409e533b2
Kendrenogen <senabuduh4ckerz@gmail.com>
2024-04-28 14:14:42 -0600

    Revert "initrd: switch initrd loading to struct file based APIs"
    This reverts commit 5020203612e5b4d4682d386e98a935c10e7acffa.
    
    LGE touchscreen drivers use ksys_lseek, which is made static in the above
    commit, so revert it.

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -1236,6 +1236,7 @@ asmlinkage long sys_ni_syscall(void);
 ssize_t ksys_write(unsigned int fd, const char __user *buf, size_t count);
 int ksys_fchown(unsigned int fd, uid_t user, gid_t group);
 int ksys_ioctl(unsigned int fd, unsigned int cmd, unsigned long arg);
+off_t ksys_lseek(unsigned int fd, off_t offset, unsigned int whence);
 ssize_t ksys_read(unsigned int fd, char __user *buf, size_t count);
 void ksys_sync(void);
 int ksys_unshare(unsigned long unshare_flags);
